2886c3b6eaad34a93dcf1c777cfbb45de7ef02ec
Daniela Guzman <danalguzmanm@correo.udistrital.edu.co>
2022-02-22 17:59:10 -0500

    Eliminar

--- docs/js/app.js
+++ docs/js/app.js
@@ -70,12 +70,20 @@ const crearIngresoHTML = (ingreso) =>{
             </div>
             <div class="elemento_eliminar">
                 <button class="elemento_eliminar--btn">
-                    <ion-icon name="close-circle-outline"></ion-icon>
+                    <ion-icon name="close-circle-outline"
+                    onclick="eliminarIngreso(${ingreso.id})"></ion-icon>
                 </button>
             </div>
         </div>
     </div>`;   
     return ingresoHTML;
+}
+
+const eliminarIngreso = (id) =>{
+    let indicieEliminar = ingresos.findIndex(ingreso => ingreso.id === id)
+    ingresos.splice(indicieEliminar,1);
+    cargarCabecero();
+    cargarIngresos();
 }
 
 const cargarEgresos = () =>{
@@ -101,11 +109,19 @@ const crearEgresoHTML = (egreso) =>{
             </div>
             <div class="elemento_eliminar">
                 <button class="elemento_eliminar--btn">
-                    <ion-icon name="close-circle-outline"></ion-icon>
+                    <ion-icon name="close-circle-outline"
+                    onclick="eliminarEgreso(${egreso.id})"></ion-icon>
                 </button>
             </div>
         </div>
     </div>
     `;
     return egresoHTML;
+}
+
+const eliminarEgreso = (id) =>{
+    let indicieEliminar = egresos.findIndex(egreso => egreso.id === id)
+    egresos.splice(indicieEliminar,1);
+    cargarCabecero();
+    cargarEgresos();
 }
